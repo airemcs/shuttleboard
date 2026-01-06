8f572c53b7a22e047bca11c17368360311c7ae4a
Airelle M. <its.airemcs@gmail.com>
2026-01-06 15:30:09 +0800

    Adjust Navbar to Web

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,21 @@
 import { RxHamburgerMenu } from "react-icons/rx";
+import Button from "@/components/Button";
 
 export default function Navbar() {
   return(
-    <div className="w-screen flex items-center justify-between p-4 bg-white border-b border-[#E1E5EA]">
+    <div className="w-full flex items-center justify-between p-4 bg-white border-b border-[#E1E5EA]">
       <div className="flex items-center gap-2.5">
-        <div className="bg-primary-green size-10 rounded-lg"></div>
-        <span className="font-sf-bold text-lg tracking-wide">Shuttleboard PH</span>
+        <div className="bg-primary-green size-10 lg:size-12 rounded-lg"></div>
+        <span className="font-sf-bold text-lg lg:text-2xl">Shuttleboard PH</span>
       </div>
 
-      <RxHamburgerMenu className="size-6" />
+      <RxHamburgerMenu className="size-6 lg:hidden" />
+
+      <div className="hidden lg:flex gap-4">
+        <Button variant="ghost" text="Home"/>
+        <Button variant="secondary" text="All Events"/>
+        <Button variant="primary" text="Submit Event"/>
+      </div>
     </div>
   )
 }
