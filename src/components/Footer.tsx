d38a1326c7d4d6aae64cd89ae0da5e7c1d781363
Airelle M. <its.airemcs@gmail.com>
2026-01-06 19:32:12 +0800

    Adjust Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="py-9 font-sf-regular text-sm flex justify-between text-tertiary-black">
+    <div className="py-4 lg:py-9 font-sf-regular text-sm flex justify-between text-tertiary-black">
       <span className="hidden lg:inline">© Shuttleboard • PH Badminton Events Directory • A community resource for Filipino badminton players.</span>
       <span className="lg:hidden">© Shuttleboard</span>
       <span>Last Updated: January 6, 2026</span>
