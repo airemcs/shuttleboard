21ba5085a2cbb4629d16f393ccbfc351a932a8ff
Airelle M. <its.airemcs@gmail.com>
2026-01-06 14:54:49 +0800

    Create Mobile Navbar Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar"
 
 export default function App() {
   return(
-  <div className="max-w-5/6">
+  <div className="h-screen bg-surface">
     <Navbar />
   </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+
 export default function Navbar() {
   return(
-    <>
-      <h1 className="text-lg font-sf-regular text-secondary">Welcome to Shuttleboard!</h1>
-    </>
+    <div className="w-screen flex items-center justify-between p-4 bg-white border-b border-[#E1E5EA]">
+      <div className="flex items-center gap-2.5">
+        <div className="bg-primary-green size-10 rounded-lg"></div>
+        <span className="font-sf-bold text-lg tracking-wide">Shuttleboard PH</span>
+      </div>
+
+      <RxHamburgerMenu className="size-6" />
+    </div>
   )
 }
