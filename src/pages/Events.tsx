d0a96545412cbe541db5d1bd3c30c3c10c7de7e3
Airelle M. <its.airemcs@gmail.com>
2026-01-09 12:11:48 +0800

    Adjustments

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -238,7 +238,7 @@ export default function Events() {
           </span>
           
           {filteredEvents.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {filteredEvents.map((event) => (
                 <Card
                   key={event.id}
